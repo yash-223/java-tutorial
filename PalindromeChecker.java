f0a60c432fe683dbf0bd4ad7b0723fb4f533c291
yash sali <saliyash038@gmail.com>
2025-02-08 22:30:40 +0530

    add palidromchecker

---
+++ PalindromeChecker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
